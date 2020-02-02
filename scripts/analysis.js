03cd4b01502ed637fdd79759f053a5ea34edce72
goubin510 <goubin510@gmail.com>
2020-02-02 23:12:49 +0100

    max date

--- scripts/analysis.js
+++ scripts/analysis.js
@@ -31,27 +31,29 @@ module.exports = {
     // }
 
     current: function (db, res, identifiant) {
-		var dateY = new Date();
-		var dateM = new Date();
-		var dateT = new Date();
+		var dateYTD = new Date();
+		var dateMTD = new Date();
+		var dateTD = new Date();
+		var dateRef = new Date();
 
 		var json = {};
 		var YTD = false, MTD = false, TD = false;
 
-		dateY.setMonth(0);
-		dateY.setDate(0);
-		dateY.setHours(23,59,59,999);
+		dateYTD.setMonth(0);
+		dateYTD.setDate(0);
+		dateYTD.setHours(23,59,59,999);
 
-		dateM.setDate(0);
-		dateM.setHours(23,59,59,999);
+		dateMTD.setDate(0);
+		dateMTD.setHours(23,59,59,999);
 
-		dateT.setHours(00,00,00,000);
+		dateTD.setHours(00,00,00,000);
 
 		db.aggregate( [
 						{
 							$match: {
 									date: {
-											$gte: dateY
+											$gte: dateYTD,
+											$lte: dateRef,
 										},
 									identificationStr: identifiant
 								}
@@ -85,7 +87,8 @@ module.exports = {
 						{
 							$match: {
 									date: {
-											$gte: dateM
+											$gte: dateMTD,
+											$lte: dateRef,
 										},
 									identificationStr: identifiant
 								}
@@ -119,7 +122,8 @@ module.exports = {
 						{
 							$match: {
 									date: {
-											$gte: dateT
+											$gte: dateTD,
+											$lte: dateRef,
 										},
 									identificationStr: identifiant
 								}
